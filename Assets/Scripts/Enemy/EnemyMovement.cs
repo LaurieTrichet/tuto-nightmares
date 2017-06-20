7deec66f857e4078c4940a3f7ac98300cb6b8017
Laurie Trichet <laurie.trichet@gmail.com>
2017-06-20 22:04:33 +1000

    player can shoot at enemy

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,32 +4,32 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour
 {
     Transform player;
-    //PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
     UnityEngine.AI.NavMeshAgent nav;
 
-    // Animator animator;
+    Animator animator;
 
     void Awake ()
     {
         player = GameObject.FindGameObjectWithTag ("Player").transform;
-        //playerHealth = player.GetComponent <PlayerHealth> ();
-       // enemyHealth = GetComponent <EnemyHealth> ();
+        playerHealth = player.GetComponent <PlayerHealth> ();
+       enemyHealth = GetComponent <EnemyHealth> ();
         nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
-        // animator = GetComponent <Animator>();
+        animator = GetComponent <Animator>();
     }
 
 
     void Update ()
     {
-      //  if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-      //  {
+       if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+       {
             nav.SetDestination (player.position);
-      //  }
-        //else
-        //{
-            //animator.SetTrigger("PlayerDead")
-       //    nav.enabled = false;
-        //}
+       }
+        else
+        {
+            animator.SetTrigger("PlayerDead");
+            nav.enabled = false;
+        }
     }
 }
